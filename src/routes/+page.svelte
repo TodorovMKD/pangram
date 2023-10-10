b192fbeaaf920a5571aaf2b31535bcdf3c07d8c6
Simon Todorov <simon.todorov@mathema.de>
2023-10-10 09:54:22 +0200

    Function done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,16 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	let alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+	function isPangram(inputString: string): boolean {
+		inputString = inputString.toLowerCase();
+		let processedString = inputString.replace(/[^a-zA-Z]/g, '');
+
+		for (let element of alphabet.split('')) {
+			if (!processedString.includes(element)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+</script>
